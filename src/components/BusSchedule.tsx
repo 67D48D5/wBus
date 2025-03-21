817adbacf69f120c710af6b3d5b835337a3c8c21
F8DC102 <kernel@yonsei.ac.kr>
2025-03-21 23:36:11 +0900

    Update message

--- src/components/BusSchedule.tsx
+++ src/components/BusSchedule.tsx
@@ -152,9 +152,8 @@ export default function BusSchedule({ routeId }: Props) {
           </p>
         ) : firstDeparture ? (
           <p className="mt-2 text-sm text-gray-700 font-bold leading-snug">
-            🚫 지금은 학관 정류장에서 출발하지 않아요.
-            <br />
-            첫차는 <span className="text-blue-700">{firstDeparture}</span>
+            🚫 지금은 학관 정류장에서 출발하지 않아요. 첫차는
+            <span className="text-blue-700"> {firstDeparture}</span>
             입니다.
           </p>
         ) : (
