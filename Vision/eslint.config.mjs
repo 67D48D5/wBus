5dd09f17c4b02319eb793cb1b0a154644889fce7
67D48D5 <kernel@yonsei.ac.kr>
2026-01-26 15:54:43 +0900

    Migrate linter to `ESLint`

---
+++ Vision/eslint.config.mjs
@@ -0,0 +1,15 @@
+// eslint.config.mjs
+
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+// import { fileURLToPath } from "node:url";
+
+// import path from "node:path";
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+export default defineConfig([{
+    extends: [...nextCoreWebVitals, ...nextTypescript],
+}]);
